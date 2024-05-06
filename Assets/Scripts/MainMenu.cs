1d3335edabe161b273d16e71a475de5483c80c6f
Agilham <13521118@std.stei.itb.ac.id>
2024-05-06 15:04:28 +0700

    feat: add button navigation

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +8,21 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene("Main");
+    }
+
+    public void GoToStatistics()
+    {
+        SceneManager.LoadScene("Statistics");
+    }
+
+    public void GoToSettings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
     }
 
     public void QuitGame()
