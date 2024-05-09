70270c0e54aa9075461c860635d1e2723ed57dfe
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2024-05-09 23:36:01 +0700

    feat: enemy buff DOT area

---
+++ Assets/Scripts/Enemy/Buffs/DOT.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class DOT : MonoBehaviour
+{
+    public int damagePerSecond = 10;
+
+    private bool isDamaging = false;
+    private float lastDamageTime;
+    private Collider playerCollider;
+
+    void Start()
+    {
+        lastDamageTime = Time.time;
+    }
+
+    void Update()
+    {
+        // Check if it's time to damage again
+        if (isDamaging && Time.time - lastDamageTime > 1f)
+        {
+            DamageOverTime();
+            lastDamageTime = Time.time;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerCollider = other;
+            isDamaging = true;
+            lastDamageTime = Time.time;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerCollider = null;
+            isDamaging = false;
+        }
+    }
+
+    void DamageOverTime()
+    {
+        if (playerCollider == null) return;
+
+        // Assuming you've set damage on your projectiles
+        PlayerHealth playerHealth = playerCollider.GetComponent<PlayerHealth>();
+        if (playerHealth != null)
+        {
+            playerHealth.TakeDamage(damagePerSecond);
+        }
+    }
+}
