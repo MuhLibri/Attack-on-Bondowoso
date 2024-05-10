7416ca53e1c1ba9017ec6dccb8a6d4ac1c8fdbb8
Muhammad Equilibrie Fajria <104043362+MuhLibri@users.noreply.github.com>
2024-05-10 14:49:59 +0700

    feat: add orb cheat

--- Assets/Scripts/Cheat/DebugController.cs
+++ Assets/Scripts/Cheat/DebugController.cs
@@ -5,10 +5,19 @@ public class DebugController : MonoBehaviour
 {
     bool showConsole;
     string input = "";
+    public GameObject player;
+    public GameObject orbDamage;
+    public GameObject orbHealth;
+    public GameObject orbSpeed;
     public static DebugCommand NO_DAMAGE;
     public static DebugCommand ONE_HIT_KILL;
     public static DebugCommand MOTHERLODE;
     public static DebugCommand TWICE_SPEED;
+    public static DebugCommand FULL_HP_PET;
+    public static DebugCommand KILL_PET;
+    public static DebugCommand ORB_DAMAGE;
+    public static DebugCommand ORB_HEALTH;
+    public static DebugCommand ORB_SPEED;
     public static DebugCommand SKIP;
     public List<DebugCommand> commandList;
 
@@ -57,6 +66,50 @@ public class DebugController : MonoBehaviour
             }
         });
 
+        FULL_HP_PET = new("full_hp_pet", "Make the pet unkillable", "full_hp_pet", () => {
+            // TO DO Implement
+            if (PlayerHealth.IsNoDamage()) {
+                PlayerHealth.DeactivateNoDamage();
+                Debug.Log("Full Hp Pet deactivated");
+            }
+            else {
+                PlayerHealth.ActivateNoDamage();
+                Debug.Log("Full Hp Pet activated");
+            }            
+        });
+
+        KILL_PET = new("kill_pet", "Instantly kill all pet", "kill_pet", () => {
+            // TO DO Implement
+            Debug.Log("Kill Pet activated");
+        });
+
+        ORB_DAMAGE = new("orb_damage", "Instantly spawn an increase damage orb", "orb_damage", () => {
+            Transform playerTransform = player.GetComponent<Transform>();
+            Vector3 orbPosition = playerTransform.position;
+            Quaternion orbRotation = playerTransform.rotation;
+            orbPosition.x += 3;
+            Instantiate(orbDamage, orbPosition, orbRotation);
+            Debug.Log("Orb Damage activated");
+        });
+
+        ORB_HEALTH = new("orb_health", "Instantly spawn a restore health orb", "orb_health", () => {
+            Transform playerTransform = player.GetComponent<Transform>();
+            Vector3 orbPosition = playerTransform.position;
+            Quaternion orbRotation = playerTransform.rotation;
+            orbPosition.x += 3;
+            Instantiate(orbHealth, orbPosition, orbRotation);
+            Debug.Log("Orb Health activated");
+        });
+        
+        ORB_SPEED = new("orb_speed", "Instantly spawn an increase speed orb", "orb_speed", () => {
+            Transform playerTransform = player.GetComponent<Transform>();
+            Vector3 orbPosition = playerTransform.position;
+            Quaternion orbRotation = playerTransform.rotation;
+            orbPosition.x += 3;
+            Instantiate(orbSpeed, orbPosition, orbRotation);
+            Debug.Log("Orb Speed activated");
+        });
+        
         SKIP = new("skip", "Skip current quest", "skip", () => {
             QuestManager.SkipCurrentQuest();
             Debug.Log("Skip quest activated");
@@ -67,6 +120,11 @@ public class DebugController : MonoBehaviour
             ONE_HIT_KILL,
             MOTHERLODE,
             TWICE_SPEED,
+            FULL_HP_PET,
+            KILL_PET,
+            ORB_DAMAGE,
+            ORB_HEALTH,
+            ORB_SPEED,
             SKIP
         };
     }
