61e7135002c610827111ad38030137fcf62fac74
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2024-05-10 09:17:22 +0700

    feat: attack down debuff

--- Assets/Scripts/Player/PlayerWeaponState.cs
+++ Assets/Scripts/Player/PlayerWeaponState.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerWeaponState : MonoBehaviour
-{ 
+{
     /* for documentation purposes:
         0 = sword
         1 = shotgun
     
     */
 
-    public enum WeaponType {
+    public enum WeaponType
+    {
         None = -1,
         MachinePistol = 0,
         Shotgun = 1,
@@ -50,8 +51,10 @@ public class PlayerWeaponState : MonoBehaviour
 
         weapons = new List<GameObject>();
         allChildren = transform.GetComponentsInChildren<Transform>();
-        foreach(Transform child in allChildren){
-            if(child.tag == "Weapon"){
+        foreach (Transform child in allChildren)
+        {
+            if (child.tag == "Weapon")
+            {
                 weapons.Add(child.gameObject);
             }
         }
@@ -72,9 +75,12 @@ public class PlayerWeaponState : MonoBehaviour
         petDamageBoost();
     }
 
-    GameObject GetWeaponWithName(string weaponName){
-        foreach(GameObject weapon in weapons){
-            if(weapon.name == weaponName){
+    GameObject GetWeaponWithName(string weaponName)
+    {
+        foreach (GameObject weapon in weapons)
+        {
+            if (weapon.name == weaponName)
+            {
                 return weapon;
             }
         }
@@ -82,55 +88,69 @@ public class PlayerWeaponState : MonoBehaviour
         return null;
     }
 
-    void WeaponChangeInput(){
+    void WeaponChangeInput()
+    {
         float scrollInput = Input.GetAxis("Mouse ScrollWheel");
-        int scrollSign = (int) Mathf.Sign(scrollInput);
-        int weaponValue = (int) currentWeapon;
+        int scrollSign = (int)Mathf.Sign(scrollInput);
+        int weaponValue = (int)currentWeapon;
 
-        if(Input.GetKeyDown(KeyCode.Alpha1)){
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
             weaponValue = 0;
         }
-        else if(Input.GetKeyDown(KeyCode.Alpha2)){
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
             weaponValue = 1;
         }
-        else if(Input.GetKeyDown(KeyCode.Alpha3)){
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
             weaponValue = 2;
         }
-        else if(Input.GetKeyDown(KeyCode.X)){
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
             weaponValue = -1;
         }
-        else if(scrollInput != 0) {
+        else if (scrollInput != 0)
+        {
             weaponValue += scrollSign;
             weaponValue = weaponValue % weapons.Count;
-        }  
+        }
 
         previousWeapon = currentWeapon;
-        currentWeapon = (WeaponType) weaponValue;
+        currentWeapon = (WeaponType)weaponValue;
         currentWeaponObject = GetWeaponWithName(currentWeapon.ToString());
     }
 
-    void WeaponStateUpdate(){
-        if(currentWeapon != previousWeapon){
-            if(currentWeapon != WeaponType.None){
+    void WeaponStateUpdate()
+    {
+        if (currentWeapon != previousWeapon)
+        {
+            if (currentWeapon != WeaponType.None)
+            {
                 ActivateWeapon();
                 DeactivateOtherWeapons();
-                
-                if (currentWeapon == WeaponType.Sword){
+
+                if (currentWeapon == WeaponType.Sword)
+                {
                     playerAnimator.SetLayerWeight(1, 0f);
                     playerAnimator.SetBool("Equip Sword", true);
                 }
-                else if(currentWeapon == WeaponType.Shotgun){
+                else if (currentWeapon == WeaponType.Shotgun)
+                {
                     playerAnimator.SetLayerWeight(1, 1f);
                     playerAnimator.SetBool("Equip Sword", false);
                 }
-                else if(currentWeapon == WeaponType.MachinePistol){
+                else if (currentWeapon == WeaponType.MachinePistol)
+                {
                     playerAnimator.SetLayerWeight(1, 0f);
                     playerAnimator.SetBool("Equip Sword", false);
                 }
             }
 
-            else { // if no weapon is selected
-                foreach(GameObject weapon in weapons){
+            else
+            { // if no weapon is selected
+                foreach (GameObject weapon in weapons)
+                {
                     weapon.SetActive(false);
                 }
 
@@ -140,152 +160,186 @@ public class PlayerWeaponState : MonoBehaviour
         }
     }
 
-    void DeactivateOtherWeapons(){
-        foreach(GameObject weapon in weapons){
-            if(weapon != currentWeaponObject){
+    void DeactivateOtherWeapons()
+    {
+        foreach (GameObject weapon in weapons)
+        {
+            if (weapon != currentWeaponObject)
+            {
                 weapon.SetActive(false);
             }
         }
     }
 
-    void ActivateWeapon(){
+    void ActivateWeapon()
+    {
         currentWeaponObject.SetActive(true);
-        
-        if(currentWeapon == WeaponType.Sword){
+
+        if (currentWeapon == WeaponType.Sword)
+        {
             playerAnimator.SetLayerWeight(1, 0f);
         }
-        else if(currentWeapon == WeaponType.Shotgun){
+        else if (currentWeapon == WeaponType.Shotgun)
+        {
             playerAnimator.SetLayerWeight(1, 1f);
         }
     }
 
-    void AimingCheck() {
+    void AimingCheck()
+    {
 
         bool aimingInput = Input.GetButton("Fire2");
         playerAnimator.SetBool("Aim", aiming);
 
         if (aimingInput && (currentWeapon == WeaponType.Shotgun || currentWeapon == WeaponType.MachinePistol))
-        {   
+        {
             aiming = aimingInput;
             playerAnimator.SetLayerWeight(2, 1f);
         }
         else
-        {   
+        {
             aiming = aimingInput;
             playerMovement.moveAllowed = true;
             playerAnimator.SetLayerWeight(2, 0f);
         }
-        
+
     }
 
-    void ResrictMovement(){
-        if(aiming || onAttack.attacking){
+    void ResrictMovement()
+    {
+        if (aiming || onAttack.attacking)
+        {
             playerMovement.moveAllowed = false;
         }
-        else{
+        else
+        {
             playerMovement.moveAllowed = true;
         }
     }
 
-    public void DamageBoost(int percentage) {
-        if(damageOrbCount <= orbCountMax) {
+    public void DamageBoost(int percentage)
+    {
+        if (damageOrbCount <= orbCountMax)
+        {
             damageOrbCount += 1;
 
-            foreach(GameObject weapon in weapons){
-                if(weapon.name == "MachinePistol"){
+            foreach (GameObject weapon in weapons)
+            {
+                if (weapon.name == "MachinePistol")
+                {
                     MachinePistolAttack machinePistolAttack = weapon.GetComponent<MachinePistolAttack>();
                     int damageBoost = machinePistolAttack.defaultDamage * percentage / 100;
 
-                    machinePistolAttack.damage = machinePistolAttack.damage + damageBoost; 
+                    machinePistolAttack.damage = machinePistolAttack.damage + damageBoost;
                     machinePistolAttack.defaultDamage = machinePistolAttack.defaultDamage + damageBoost;
                 }
-                else if(weapon.name == "Shotgun"){
+                else if (weapon.name == "Shotgun")
+                {
                     ShotgunAttack shotgunAttack = weapon.GetComponent<ShotgunAttack>();
                     int damageBoost = shotgunAttack.defaultDamage * percentage / 100;
 
-                    shotgunAttack.damage = shotgunAttack.damage + damageBoost; 
+                    shotgunAttack.damage = shotgunAttack.damage + damageBoost;
                     shotgunAttack.defaultDamage = shotgunAttack.defaultDamage + damageBoost;
                 }
-                else if(weapon.name == "Sword"){
+                else if (weapon.name == "Sword")
+                {
                     SwordScript swordAttack = weapon.GetComponent<SwordScript>();
                     int damageBoost = swordAttack.defaultDamage * percentage / 100;
 
-                    swordAttack.damage = swordAttack.damage + damageBoost; 
+                    swordAttack.damage = swordAttack.damage + damageBoost;
                     swordAttack.defaultDamage = swordAttack.defaultDamage + damageBoost;
                 }
             }
         }
     }
 
-    void setDefaultDamage(){
-        foreach(GameObject weapon in weapons){
-            if(weapon.name == "MachinePistol"){
+    public void setDefaultDamage()
+    {
+        foreach (GameObject weapon in weapons)
+        {
+            if (weapon.name == "MachinePistol")
+            {
                 MachinePistolAttack machinePistolAttack = weapon.GetComponent<MachinePistolAttack>();
                 machinePistolAttack.damage = machinePistolAttack.defaultDamage;
             }
-            else if(weapon.name == "Shotgun"){
+            else if (weapon.name == "Shotgun")
+            {
                 ShotgunAttack shotgunAttack = weapon.GetComponent<ShotgunAttack>();
                 shotgunAttack.damage = shotgunAttack.defaultDamage;
             }
-            else if(weapon.name == "Sword"){
+            else if (weapon.name == "Sword")
+            {
                 SwordScript swordAttack = weapon.GetComponent<SwordScript>();
                 swordAttack.damage = swordAttack.defaultDamage;
             }
         }
     }
 
-    void addDamage(int percentage, int multiplier){
-        foreach(GameObject weapon in weapons){
-            if(weapon.name == "MachinePistol"){
+    void addDamage(int percentage, int multiplier)
+    {
+        foreach (GameObject weapon in weapons)
+        {
+            if (weapon.name == "MachinePistol")
+            {
                 MachinePistolAttack machinePistolAttack = weapon.GetComponent<MachinePistolAttack>();
                 int damageBoost = machinePistolAttack.defaultDamage * percentage / 100;
 
-                machinePistolAttack.damage = machinePistolAttack.damage + (damageBoost * multiplier); 
+                machinePistolAttack.damage = machinePistolAttack.damage + (damageBoost * multiplier);
             }
-            else if(weapon.name == "Shotgun"){
+            else if (weapon.name == "Shotgun")
+            {
                 ShotgunAttack shotgunAttack = weapon.GetComponent<ShotgunAttack>();
                 int damageBoost = shotgunAttack.defaultDamage * percentage / 100;
 
-                shotgunAttack.damage = shotgunAttack.damage + damageBoost+ (damageBoost * multiplier); 
+                shotgunAttack.damage = shotgunAttack.damage + damageBoost + (damageBoost * multiplier);
             }
-            else if(weapon.name == "Sword"){
+            else if (weapon.name == "Sword")
+            {
                 SwordScript swordAttack = weapon.GetComponent<SwordScript>();
                 int damageBoost = swordAttack.defaultDamage * percentage / 100;
 
-                swordAttack.damage = swordAttack.damage + damageBoost+ (damageBoost * multiplier); 
+                swordAttack.damage = swordAttack.damage + damageBoost + (damageBoost * multiplier);
             }
         }
     }
 
-    void decreaseDamage(int percentage, int multiplier){
-        foreach(GameObject weapon in weapons){
-            if(weapon.name == "MachinePistol"){
+    public void decreaseDamage(int percentage, int multiplier)
+    {
+        foreach (GameObject weapon in weapons)
+        {
+            if (weapon.name == "MachinePistol")
+            {
                 MachinePistolAttack machinePistolAttack = weapon.GetComponent<MachinePistolAttack>();
                 int damageBoost = machinePistolAttack.defaultDamage * percentage / 100;
 
-                machinePistolAttack.damage = machinePistolAttack.damage - (damageBoost * multiplier); 
+                machinePistolAttack.damage = machinePistolAttack.damage - (damageBoost * multiplier);
             }
-            else if(weapon.name == "Shotgun"){
+            else if (weapon.name == "Shotgun")
+            {
                 ShotgunAttack shotgunAttack = weapon.GetComponent<ShotgunAttack>();
                 int damageBoost = shotgunAttack.defaultDamage * percentage / 100;
 
-                shotgunAttack.damage = shotgunAttack.damage - damageBoost+ (damageBoost * multiplier); 
+                shotgunAttack.damage = shotgunAttack.damage - damageBoost + (damageBoost * multiplier);
             }
-            else if(weapon.name == "Sword"){
+            else if (weapon.name == "Sword")
+            {
                 SwordScript swordAttack = weapon.GetComponent<SwordScript>();
                 int damageBoost = swordAttack.defaultDamage * percentage / 100;
 
-                swordAttack.damage = swordAttack.damage - damageBoost+ (damageBoost * multiplier); 
+                swordAttack.damage = swordAttack.damage - damageBoost + (damageBoost * multiplier);
             }
         }
     }
 
 
-    public void petDamageBoost(){
-        if (petDamageCount == 0) {
+    public void petDamageBoost()
+    {
+        if (petDamageCount == 0)
+        {
             setDefaultDamage();
         }
-        else {
+        else
+        {
             addDamage(petDamageBoostPercentage, petDamageCount);
         }
     }
